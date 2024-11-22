4d040c2938234c1d8af17d9bc8167ea44308fdb1
Yar4e <167015075+yar4e4@users.noreply.github.com>
2024-11-22 12:03:36 +0300

    Delete rest-api-test-task-t3/trainee_project directory

--- rest-api-test-task-t3/trainee_project/src/main/java/com/romash_co/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.romash_co.repository;
-
-import com.romash_co.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    // Дополнительные методы, если нужны
-}
